fec6a6a69e724a298ced4dc2b0198d44ca9d7d33
delyan-kirov <delyan.k.kirov@gmail.com>
2024-02-15 18:34:55 +0200

    Adding cli to the dictionary app

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,10 @@ use rusqlite::{Connection, Result};
 use serde::{Deserialize, Serialize};
 use std::fmt;
 
-#[allow(warnings)]
 use std::fs::File;
 use std::io::{self};
 use std::path::Path;
+use std::str::FromStr;
 
 #[derive(Debug, Serialize, Deserialize, PartialEq, Eq)]
 struct Word {
@@ -64,8 +64,21 @@ impl fmt::Display for Word {
     }
 }
 
+enum InputErr {
+    Empty,
+    Invalid,
+}
+
+impl fmt::Display for InputErr {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            InputErr::Invalid => write!(f, "Invalid"),
+            InputErr::Empty => write!(f, "Invalid"),
+        }
+    }
+}
+
 #[derive(Debug, Serialize, Deserialize, PartialEq, Eq)]
-#[allow(warnings)]
 enum Category {
     Verb,
     Noun,
@@ -90,11 +103,37 @@ impl fmt::Display for Category {
         }
     }
 }
+impl FromStr for Category {
+    type Err = InputErr;
+
+    fn from_str(s: &str) -> std::result::Result<Self, Self::Err> {
+        match s.to_lowercase().as_str() {
+            "verb" => Ok(Category::Verb),
+            "preposition" => Ok(Category::Preposition),
+            "pronoun" => Ok(Category::Pronoun),
+            "conjunction" => Ok(Category::Conjunction),
+            "adverb" => Ok(Category::Adverb),
+            "noun" => Ok(Category::Noun),
+            "article" => Ok(Category::Article),
+            "participle" => Ok(Category::Participle),
+            "" => Err(InputErr::Empty),
+            _ => Err(InputErr::Invalid),
+        }
+    }
+}
 
 #[derive(Debug, Serialize, Deserialize, PartialEq, Eq)]
 struct Etymology {
     source: String,
     description: String,
+}
+impl Default for Etymology {
+    fn default() -> Self {
+        Etymology {
+            source: "".to_string(),
+            description: "".to_string(),
+        }
+    }
 }
 impl fmt::Display for Etymology {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
@@ -119,6 +158,7 @@ fn read_word(dir_path: &Path) -> io::Result<String> {
     let file = File::open(file_path)?;
     dbg!(file);
     return io::Result::Ok("".to_string());
+    todo!()
 }
 
 // TODO: Implement CLI
@@ -160,8 +200,6 @@ mod tests {
         println! {"{:}", new_word};
     }
 
-    // TODO: Create a funtion that reads words from files
-
     #[test]
     fn test2() -> Result<(), Box<dyn std::error::Error>> {
         let word_file = Path::new("./tests/jo.yaml");
@@ -175,9 +213,9 @@ mod tests {
         let words: Vec<Word> = serde_yaml::from_str(&word_yaml)?;
 
         // Print the deserialized words
-        for word in &words {
-            println!("{:}", word);
-        }
+        // for word in &words {
+        //     println!("{:}", word);
+        // }
 
         let new_word = Word {
             spelling: "jo".to_string(),
@@ -203,7 +241,244 @@ mod tests {
 
         Ok(())
     }
+
+    #[test]
+    fn test3() -> Result<(), Box<dyn std::error::Error>> {
+        let word_file = Path::new("./tests/multiple_words.yaml");
+        let mut file = File::open(word_file)?;
+
+        // Read the contents of the file into a String
+        let mut word_yaml = String::new();
+        file.read_to_string(&mut word_yaml)?;
+
+        // Deserialize the YAML string into a Vec<Word>
+        let words: Vec<Word> = serde_yaml::from_str(&word_yaml)?;
+
+        println!("{}", words[1]);
+
+        assert_eq!(2, words.len());
+
+        Ok(())
+    }
 }
+
+fn cli_word_gen() -> Word {
+    let word_spelling: String = {
+        let mut spelling = String::new();
+        println!("Provide the spelling of the word");
+        io::stdin()
+            .read_line(&mut spelling)
+            .expect("Failed to read input");
+        spelling.trim().to_string().to_lowercase()
+    };
+
+    let word_cat = {
+        println!("Provide the lexical category of the word");
+        let mut _word_cat = Category::Verb;
+        loop {
+            let mut word_cat_str = String::new();
+            io::stdin()
+                .read_line(&mut word_cat_str)
+                .expect("Failed to read input");
+
+            let word_cat_str = word_cat_str.trim().to_string().to_lowercase();
+
+            match Category::from_str(&word_cat_str) {
+                Ok(cat) => {
+                    _word_cat = cat;
+                    break;
+                }
+                Err(InputErr::Empty) => {
+                    println!("The provided input is empty. Try again");
+                    continue;
+                }
+                Err(InputErr::Invalid) => {
+                    println!("The provided input is not valid");
+                    println!("The category of a word must be one of: ...");
+                    continue;
+                }
+            }
+        }
+        _word_cat
+    };
+
+    let word_etym: Etymology = {
+        let mut etymology = Etymology::default();
+        println!("Provide etymology");
+
+        loop {
+            println!("Provide the source of the word");
+            let mut word_source_str = String::new();
+            io::stdin()
+                .read_line(&mut word_source_str)
+                .expect("Failed to read input");
+            let word_source_str = word_source_str.trim().to_lowercase();
+            if word_source_str.is_empty() {
+                println!("The etymological source is empty which is not allowed.");
+                println!("Provide a language source");
+                continue;
+            }
+
+            println!("Provide description");
+            let mut word_description_str = String::new();
+            io::stdin()
+                .read_line(&mut word_description_str)
+                .expect("Failed to read input");
+            let word_description_str = word_description_str.trim().to_lowercase();
+            if word_description_str.is_empty() {
+                println!("The etymological description is empty which is not allowed");
+                println!("Please provide a description");
+                continue;
+            }
+
+            // Assign values to the etymology struct
+            etymology.source = word_source_str.to_string();
+            etymology.description = word_description_str.to_string();
+            break;
+        }
+        etymology
+    };
+
+    let word_def: Vec<String> = {
+        let mut word_definitions = vec![];
+        let mut word_def_str = String::new();
+        println!("Please provide the definition of the word");
+        println!("More than one definition can be given");
+
+        loop {
+            io::stdin()
+                .read_line(&mut word_def_str)
+                .expect("Failed to read input");
+            let word_def_str = word_def_str.trim().to_lowercase();
+            if word_def_str.is_empty() {
+                println!(
+                    "The definition is empty which is not allowed. Please provide a definition"
+                );
+                continue;
+            } else {
+                word_definitions.push(word_def_str.to_string());
+                println!("Would you like to provide more definitions? Yes/No");
+                let mut user_opt = String::new();
+                io::stdin()
+                    .read_line(&mut user_opt)
+                    .expect("Failed to read input");
+                let user_opt = user_opt.trim();
+                match user_opt {
+                    "No" | "no" => break,
+                    "Yes" | "yes" => continue,
+                    _ => println!("Your input is invalid. Type enter to try continue"),
+                }
+            }
+        }
+        word_definitions
+    };
+
+    let word_phon: Option<String> = {
+        println!("Provide the pronuntiation if needed or press enter to continue");
+        let mut phonology = String::new();
+        io::stdin()
+            .read_line(&mut phonology)
+            .expect("Failed to read input");
+        let phonology = phonology.trim();
+        match phonology {
+            "" => None,
+            a => Some(a.to_string()),
+        }
+    };
+
+    let word_usage: Option<String> = {
+        println!("Provide the usage if needed or press enter to continue");
+        let mut usage = String::new();
+        io::stdin()
+            .read_line(&mut usage)
+            .expect("Failed to read input");
+        let usage = usage.trim().to_lowercase();
+        match usage.as_str() {
+            "" => None,
+            a => Some(a.to_string()),
+        }
+    };
+
+    let word_derivs: Option<Vec<String>> = {
+        let mut derivatives: Vec<String> = vec![];
+        loop {
+            println!("Provide the derivative forms or press enter to continue");
+            let mut deriv_word = String::new();
+            io::stdin()
+                .read_line(&mut deriv_word)
+                .expect("Failed to read input");
+            let deriv_word = deriv_word.trim().to_lowercase();
+            if deriv_word == "" {
+                break;
+            }
+            derivatives.push(deriv_word)
+        }
+        if derivatives.is_empty() {
+            None
+        } else {
+            Some(derivatives)
+        }
+    };
+
+    let word_syn: Option<Vec<String>> = {
+        let mut synonyms: Vec<String> = vec![];
+        loop {
+            println!("Provide synonyms or press enter to continue");
+            let mut syn_word = String::new();
+            io::stdin()
+                .read_line(&mut syn_word)
+                .expect("Failed to read input");
+            let syn_word = syn_word.trim().to_lowercase();
+            if syn_word == "" {
+                break;
+            }
+            synonyms.push(syn_word)
+        }
+        if synonyms.is_empty() {
+            None
+        } else {
+            Some(synonyms)
+        }
+    };
+
+    let word_forms: Option<Vec<String>> = {
+        let mut forms: Vec<String> = vec![];
+        loop {
+            println!("Provide other forms of the word or press enter to continue");
+            let mut form_word = String::new();
+            io::stdin()
+                .read_line(&mut form_word)
+                .expect("Failed to read input");
+            let form_word = form_word.trim().to_lowercase();
+            if form_word == "" {
+                break;
+            }
+            forms.push(form_word)
+        }
+        if forms.is_empty() {
+            None
+        } else {
+            Some(forms)
+        }
+    };
+
+    Word {
+        spelling: word_spelling,
+        category: word_cat,
+        phonology: word_phon,
+        definition: word_def,
+        etymology: word_etym,
+        synonyms: word_syn,
+        usage: word_usage,
+        derivatives: word_derivs,
+        forms: word_forms,
+    }
+}
+
+// TODO: Fix the print of the word struct
+//       - [ ] Ordering
+//       - [ ] spacing
+// TODO: Add spacing when the cli prints new prompts
 
 fn main() -> Result<()> {
     let db = Path::new("./tests/example.db");
@@ -217,6 +492,9 @@ fn main() -> Result<()> {
         ",
         (),
     )?;
+
+    let new_word = cli_word_gen();
+    println!("{}", new_word);
 
     Result::Ok(())
 }
